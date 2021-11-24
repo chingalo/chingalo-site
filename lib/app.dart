fb7fbae9cdcbe8475145b2f06c6f6fb18f385301
Joseph Chingalo <profschingalo@gmail.com>
2021-11-24 18:41:38 +0300

    feat: setup default packages

--- lib/app.dart
+++ lib/app.dart
@@ -1,27 +1,34 @@
+import 'package:chingalo_site/app_state/app_theme_state/app_theme_state.dart';
+import 'package:chingalo_site/core/services/theme_service.dart';
+import 'package:chingalo_site/modules/splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Chingalo",
-      theme: ThemeData(
-        textTheme: GoogleFonts.robotoTextTheme(
-          Theme.of(context).textTheme,
-        ),
-        primaryColor: const Color(0xFF4B9F46),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: SafeArea(
-        child: Scaffold(
-          body: Container(
-            alignment: Alignment.center,
-            child: const Text("Text enter"),
-          ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppThemeState()),
+      ],
+      child: Consumer<AppThemeState>(
+        builder: (context, appThemeState, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: appThemeState.currentTheme == ThemeServices.darkTheme
+              ? ThemeData.dark().copyWith(
+                  textTheme: GoogleFonts.robotoTextTheme(
+                    Theme.of(context).textTheme,
+                  ),
+                )
+              : ThemeData.light().copyWith(
+                  textTheme: GoogleFonts.robotoTextTheme(
+                    Theme.of(context).textTheme,
+                  ),
+                ),
+          home: const Splash(),
         ),
       ),
     );
