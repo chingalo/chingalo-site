4ee00b1ef76abc53135f3c84481553204410ca82
Joseph Chingalo <profschingalo@gmail.com>
2022-01-30 08:35:48 +0300

    feat: 🎸 Add support for deducing layout to use

--- lib/core/utils/app_util.dart
+++ lib/core/utils/app_util.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class AppUtil {
   static String getYearsOfWork() {
     DateTime dateOfStartWork = DateTime.parse('2015-07-01');
@@ -8,5 +10,9 @@ class AppUtil {
   static String getHomePageInto() {
     String years = getYearsOfWork();
     return 'Holla! \n\nIts Joseph Chingalo, holds a B.Cs with Computer Science from the University of Dar-es-salaam, $years years of experience in designing, developing, and implementing information systems in Tanzania as well as other international organizations.\n\nSpecialized in the designing & development of web apps, mobile apps ( for IOS and Android) as well as cross platforms desktop apps (For Mac, Window, and Linux) as well as customization, training, and implementation of DHIS2 related projects';
+  }
+
+  static bool userMobileLayout(BuildContext context) {
+    return MediaQuery.of(context).size.shortestSide < 600;
   }
 }
